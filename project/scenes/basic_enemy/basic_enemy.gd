790f8107658f0f29aac47a79817998314e14b006
DevJac <dbuttonz@gmail.com>
2024-08-21 11:01:11 -0600

    Rat enemy, approaches and begs

---
+++ project/scenes/basic_enemy/basic_enemy.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+
+const MAX_SPEED = 75 * 60
+
+
+func _process(delta: float) -> void:
+	var to_player := vector_to_player()
+	if to_player.length_squared() > 1000:
+		velocity = to_player.normalized() * MAX_SPEED * delta
+	else:
+		velocity = Vector2.ZERO
+	move_and_slide()
+
+
+func vector_to_player() -> Vector2:
+	var player_nodes := get_tree().get_nodes_in_group('player')
+	assert(player_nodes.size() == 1)
+	var player: Node2D = player_nodes[0]
+	return player.global_position - global_position
