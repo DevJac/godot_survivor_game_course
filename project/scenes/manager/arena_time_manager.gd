49fcefd7aaaec43cfe19a9bc4da7d9e7d3674b17
DevJac <dbuttonz@gmail.com>
2024-09-30 11:58:44 -0600

    Victory screen

--- project/scenes/manager/arena_time_manager.gd
+++ project/scenes/manager/arena_time_manager.gd
@@ -1,8 +1,20 @@
 extends Node
 
 
+@export var victory_screen_scene: PackedScene
+
+
 @onready var timer = $Timer
+
+
+func _ready() -> void:
+	timer.timeout.connect(on_timer_timeout)
 
 
 func get_time_elapsed():
 	return timer.wait_time - timer.time_left
+
+
+func on_timer_timeout():
+	var victory_screen = victory_screen_scene.instantiate()
+	add_child(victory_screen)
