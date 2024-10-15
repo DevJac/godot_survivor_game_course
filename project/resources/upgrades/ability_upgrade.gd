bea8928f7f1f123282595620c3abbc9b2a5e0ada
DevJac <dbuttonz@gmail.com>
2024-10-15 10:16:18 -0600

    Prevent picking the axe ability multiple times

--- project/resources/upgrades/ability_upgrade.gd
+++ project/resources/upgrades/ability_upgrade.gd
@@ -4,4 +4,5 @@ class_name AbilityUpgrade
 
 @export var id: String
 @export var name: String
+@export var max_quantity: int = 1
 @export_multiline var description: String
--- project/scenes/manager/upgrade_manager.gd
+++ project/scenes/manager/upgrade_manager.gd
@@ -24,8 +24,20 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 	print_debug(current_upgrades)
 
 
+func get_current_upgrade_quantity(upgrade: AbilityUpgrade) -> int:
+	if upgrade.id in current_upgrades:
+		return current_upgrades[upgrade.id]['quantity']
+	else:
+		return 0
+
+
 func pick_upgrades() -> Array[AbilityUpgrade]:
 	var filtered_upgrades := upgrade_pool.duplicate()
+	filtered_upgrades = filtered_upgrades.filter(func (upgrade: AbilityUpgrade):
+		var quantity_available: bool = (
+			get_current_upgrade_quantity(upgrade) < upgrade.max_quantity)
+		return quantity_available
+	)
 	filtered_upgrades.shuffle()
 	return filtered_upgrades.slice(0, 1000)
 
@@ -37,5 +49,8 @@ func on_upgrade_selected(upgrade: AbilityUpgrade):
 func on_level_up(_current_level: int):
 	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
 	add_child(upgrade_screen_instance)
-	upgrade_screen_instance.set_ability_upgrades(pick_upgrades())
+	var upgrades := pick_upgrades()
+	if upgrades.size() == 0:
+		return
+	upgrade_screen_instance.set_ability_upgrades(upgrades)
 	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
